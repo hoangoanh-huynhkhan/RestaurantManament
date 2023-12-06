f34787a06b1a6b72e693273851b5ca4f9fdc1dd2
HUYNH VAN CHI KHAN <huynhkhan91@gmail.com>
2023-12-06 13:30:54 +0700

    Create file EmployerService.cs

---
+++ BUS/EmployerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace BUS
+{
+    public class EmployerService
+    {
+        public List<Employer> GetAll()
+        {
+            try
+            {
+                var employers = new List<Employer>();
+
+                using (DBContext db = new DBContext())
+                {
+                    employers = db.Employers.ToList();
+                }
+
+                if (employers == null)
+                {
+                    throw new Exception("Employer in database is null");
+                }
+
+                return employers.Where(x => x.is__delete == 0).ToList();
+            }catch(Exception ex)
+            {
+                throw new Exception("Erorr where is: " + ex.Message);
+            }
+        }
+    }
+}
--- GUI/frm__Login.cs
+++ GUI/frm__Login.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS;
+using DAL.Entities;
 
 namespace GUI
 {
     public partial class frm__Login : Form
     {
+        private EmployerService employerService;
+
+        List<Employer> employers;
         public frm__Login()
         {
+            employerService = new EmployerService();
+            employers = new List<Employer>();
             InitializeComponent();
+
+            this.Load += Frm__Login_Load;
+        }
+
+        private void Frm__Login_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var employermodels = employerService.GetAll();
+                foreach (Employer item in employermodels)
+                {
+                    employers.Add(item);
+                }
+                MessageBox.Show(employers.Count.ToString());
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btn__close_Click(object sender, EventArgs e)
